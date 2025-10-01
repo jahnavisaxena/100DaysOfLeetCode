bd457e8cffe1b386ede6616bc7bfe121ca864c4f
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2025-10-01 21:27:10 +0530

    Create FindMinimumOperationstoMakeAllNumbersDivisbleByThree.java

---
+++ FindMinimumOperationstoMakeAllNumbersDivisbleByThree.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int ans =0;
+        for(int n:nums){
+            if(n%3!=0)ans++;
+        }
+        return ans;
+    }
+}
